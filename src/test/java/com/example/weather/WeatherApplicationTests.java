a440a0ac4142008b023360ab8ad782a3648c6ca8
John Albert T. Presentacion <johnalbertpresentacion@gmail.com>
2024-10-23 12:28:57 +0800

    add open weather api key

--- src/test/java/com/example/weather/WeatherApplicationTests.java
+++ src/test/java/com/example/weather/WeatherApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.weather;
 
+import com.example.weather.service.WeatherService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeatherApplicationTests {
 
+	@Autowired
+	private WeatherService weatherService;
+
 	@Test
 	void contextLoads() {
+		// Add assertions to check that the weatherService bean is properly loaded
+		assert(weatherService != null);
 	}
-
 }
